e462c2aedd8f50d697f1f272d135ee73da0de4d2
Rubens Abacherli Neto <62822920+rubensabacherli@users.noreply.github.com>
2024-08-22 10:08:45 -0300

    exercicios de 4 a 7 (SEM TESTES)

--- Query.sql
+++ Query.sql
@@ -15,3 +15,28 @@ SELECT
 	Filmes.Duracao
 FROM Filmes
 WHERE Filmes.Nome = 'De Volta para o Futuro'
+
+--4
+SELECT
+	*
+FROM Filmes
+WHERE Filmes.Ano = 1997
+
+--5
+SELECT
+	*
+FROM Filmes
+WHERE Filmes.Ano > 2000
+
+--6
+SELECT
+	*
+FROM Filmes
+WHERE Filmes.Duracao > 100 and Filmes.Duracao < 150
+ORDER BY Filmes.Duracao
+
+--7
+SELECT
+	COUNT(*)
+	FROM Filmes
+GROUP BY Filmes.Ano
