17cd53b86e25233560b2836c8450d7851c6f875b
Rubens Abacherli Neto <rubensabacherli@gmail.com>
2024-08-22 12:14:32 -0300

    Concluindo a entrega dos exercicios

--- Query.sql
+++ Query.sql
@@ -32,11 +32,49 @@ WHERE Filmes.Ano > 2000
 SELECT
 	*
 FROM Filmes
-WHERE Filmes.Duracao > 100 and Filmes.Duracao < 150
+WHERE Filmes.Duracao > 100 AND Filmes.Duracao < 150
 ORDER BY Filmes.Duracao
 
 --7
 SELECT
-	COUNT(*)
-	FROM Filmes
+	Filmes.Ano,
+	COUNT(*) Quantidade
+	FROM Filmes 
 GROUP BY Filmes.Ano
+ORDER BY Quantidade DESC
+
+--8
+SELECT
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	Atores.Genero
+FROM Atores
+WHERE Atores.Genero = 'M'
+
+--9
+SELECT
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	Atores.Genero
+FROM Atores
+WHERE Atores.Genero = 'F'
+ORDER BY Atores.PrimeiroNome
+
+--10
+SELECT Filmes.Nome, Generos.Genero
+FROM Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+
+--11
+SELECT Filmes.Nome, Generos.Genero
+FROM Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE generos.Genero = 'Mistério'
+
+--12
+SELECT Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
+FROM Filmes
+JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+JOIN Atores ON Atores.Id = ElencoFilme.IdAtor;
